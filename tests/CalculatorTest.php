664ff60619bec6eb76e593f72239f5adebe8016a
afutseng <afutseng@gmail.com>
2016-10-30 10:06:06 +0800

    first fail test case

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -1,4 +1,10 @@
 <?php
-class CalculatorTest
+class CalculatorTest extends PHPUnit\Framework\TestCase
 {
+    public function testTwoNumbersCanBeAdded()
+    {
+        $calculator = new Calculator;
+        $result = $calculator->add(1, 2);
+        $this->assertEquals(3, $result);
+    }
 }
